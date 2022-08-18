61f84c56577b27334dc367a80cf54c008914ef3e
BrianS <brianseko@gmail.com>
2022-08-17 20:16:23 -0400

    initial commit - package creation

---
+++ gps_695/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gps_695',
+      version='0.1',
+      description='University of Michigan Milestone 2 Project',
+      url='https://github.com/BrianS3/MI2_drown_murphy_seko',
+      author='Drown, Gretchyn; Murphy, Patrick; Seko, Brian',
+      author_email='bseko@umich.edu',
+      license='MIT',
+      packages=['gps_695'],
+      zip_safe=False)
