2536fd93bded4d193e54907d4d28ddc75bcfc5a2
PBMurphy <88356258+PBMurphy@users.noreply.github.com>
2022-09-17 12:00:34 -0400

    Add tweet pull function to database.py

--- gps_695/database.py
+++ gps_695/database.py
@@ -75,3 +75,98 @@ def call_tweets(keyword, start_date, end_date, results):
     }
     response = requests.request("GET", url, headers=headers, data=payload).json()
     return response
+
+
+
+def load_tweets(keyword, start_date, end_date, results = 500)
+    """Pulls tweets from research project API v2
+    :param keyword: keyword of tweet for API query
+    :param start_date: start date of query, YYYY-MM-DD format, string
+    :param end_date: end date of query,  YYYY-MM-DD format, string
+    :param results: number of results to return, max 500, int
+    :return: loads tweet data to DB"""
+    from typing import List
+    import pandas as pd
+    from gps_695 import database as d
+    from gps_695 import credentials as c
+    import numpy as np
+    from pandas import json_normalize
+    import json
+    import re
+    import os
+    import mysql
+    from mysql.connector import connect, Error
+    from datetime import date
+
+    today = date.today()
+
+    datafile = d.call_tweets(keyword, start_date, end_date, results)
+    bbb = json.loads(datafile)
+
+    df_data = pd.json_normalize(bbb['data'], max_level=5)
+    retweet = df_data[df_data['referenced_tweets'].isna() == False]
+    retweet['rt_id'] = retweet['referenced_tweets'].apply(lambda x: list(list(x)[0].values())[1])
+    retweet = retweet[['id', 'rt_id']]
+
+    df_text = df_data[['id', 'text']]
+    df_text_merge = pd.merge(df_text, retweet, how='outer', left_on='id', right_on='id')
+    df_retweet = pd.json_normalize(bbb['includes']['tweets'], max_level=1)
+    df_retweet = df_retweet[['id', 'text']]
+    df_retweet.rename(columns={'id': 'rt_id_includes', 'text': 'rt_text'}, inplace=True)
+    df_text_final = pd.merge(df_text_merge, df_retweet, how='outer', left_on='rt_id', right_on='rt_id_includes')
+    df_text_final = df_text_final[['id', 'text', 'rt_id', 'rt_text']]
+    df_text_final.rename(
+        columns={'id': 'TWEET_ID', 'text': 'TWEET', 'rt_id': 'RETWEET_ID', 'rt_text': 'RETWEET_TEXT'},
+        inplace=True)
+
+    column_list = list(df_text_final.columns)
+
+    failed = pd.DataFrame(columns=column_list)
+
+
+    cnx = d.connect_to_database()
+    cursor = cnx.cursor()
+
+    count_inserted = 0
+    count_skipped = 0
+
+    print('loading...')
+    for ind, row in df_text_final.iterrows():
+        try:
+            try:
+                query = (f"""
+                INSERT INTO TWEET_TEXT (TWEET_ID, TWEET, RETWEET_ID, RETWEET_TEXT, SEARCH_TERM)
+                VALUES (
+                "{row[column_list[0]]}"
+                ,"{row[column_list[1]]}"
+                ,"{row[column_list[2]]}"
+                ,"{row[column_list[3]]}"
+                ,"{keyword}"
+                );
+                """)
+                cursor.execute(query)
+                count_inserted += 1
+            except:
+                query = (f"""
+                INSERT INTO TWEET_TEXT (TWEET_ID, TWEET, RETWEET_ID, RETWEET_TEXT, SEARCH_TERM)
+                VALUES (
+                '{row[column_list[0]]}'
+                ,'{row[column_list[1]]}'
+                ,'{row[column_list[2]]}'
+                ,'{row[column_list[3]]}'
+                ,"{keyword}"
+                );
+                """)
+                cursor.execute(query)
+                count_inserted += 1
+        except:
+            count_skipped += 1
+            # failed.loc[len(failed.index)] = list(row)
+            continue
+    cnx.commit()
+
+    # print("length of dataframe: ", len(df_text_final))
+    # print("inserted: ", count_inserted)
+    # print("skipped: ", count_skipped)
+
+    #failed.to_excel(pathway)
