75d1f929b4b4202a69ecf689962d80e1c888fc76
BrianS <43796303+BrianS3@users.noreply.github.com>
2022-10-07 19:47:24 -0400

    added tqdm to inner loop on database load

--- gps_695/database.py
+++ gps_695/database.py
@@ -323,7 +323,7 @@ def database_load(search_term):
     term_check = search_term
 
     for term in tqdm(results):
-        for i in range(1, 26):
+        for i in tqdm(range(1, 26)):
             if term != term_check:
                 first_run_param = True
                 term_check = term
